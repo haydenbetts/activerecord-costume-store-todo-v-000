6a45eb6c404fa8e9437b7aaf1156c37797986bf1
Hayden Betts <haydenbetts@gmail.com>
2018-07-12 18:31:57 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,9 @@
 class CostumeStores < ActiveRecord::Migration[5.1]
   def change
+    create_table :costume_stores do |t|
+      t.column :name, :string
+      t.column :location, :string
+      t.column :
+    end
   end
 end
