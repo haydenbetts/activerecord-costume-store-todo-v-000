bd36123aca3d030b77b05fc3094b65e4dae4a2a0
Hayden Betts <haydenbetts@gmail.com>
2018-07-12 18:32:47 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,7 @@ class CostumeStores < ActiveRecord::Migration[5.1]
       t.column :name, :string
       t.column :location, :string
       t.column :costume_inventory, :integer
-      t.column :
+      t.column :employees_count, :
     end
   end
 end
